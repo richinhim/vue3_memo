e4c8b4bd52d2a392e5b871ae25859b5e7da2eee5
sooyon lee <kwlsy666@naver.com>
2023-08-03 12:40:44 +0900

    일차커밋

---
+++ login/frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+    <div v-if="state.loggedIn">안녕하세요? 홍길동님!</div>
+    <div v-else>
+      <label for="loginId">
+        <span>아이디</span>
+        <input type="text" id="loginId" />
+      </label>
+      <label for="loginPw">
+        <span>패스워드</span>
+        <input type="text" id="loginPw" />
+      </label>
+      <hr />
+      <button>로그인</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  setup() {
+    const state = reactive({
+      loggedIn: false,
+    });
+    return { state };
+  },
+
+  components: {},
+};
+</script>
+
+<style></style>
